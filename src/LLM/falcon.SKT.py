f62099b551d69922f7ff7e5733d78f4363faf93a
Md Kowsher <ga.kowsher@gmail.com>
2024-01-05 23:11:24 -0500

    Update falcon.SKT.py

--- src/LLM/falcon.SKT.py
+++ src/LLM/falcon.SKT.py
@@ -35,14 +35,14 @@ import torch.nn as nn
 
 
 class PrefixEncoder(torch.nn.Module):
-    def __init__(self, config, transfromer):
+    def __init__(self, config, transformer):
         super().__init__()
 
         self.config = config
         self.dropout = torch.nn.Dropout(config.hidden_dropout)
-        self.transfromer=transfromer
+        self.transformer=transformer
 
-        word_embeddings = transfromer.word_embeddings
+        word_embeddings = transformer.word_embeddings
         
         tokenizer = AutoTokenizer.from_pretrained(config._name_or_path)
         
@@ -78,7 +78,7 @@ class PrefixEncoder(torch.nn.Module):
 
         inputs_embeds = self.embedding(virtual_tokens.to(device))
         inputs_embeds=self.dropout(inputs_embeds)
-        outputs = self.transfromer(
+        outputs = self.transformer(
             inputs_embeds=inputs_embeds.unsqueeze(0).repeat(batch_size, 1, 1)
         )        
         #print('working', outputs.past_key_values)
@@ -349,3 +349,180 @@ class PromptForSequenceClassification(PreTrainedModel):
             hidden_states=outputs.hidden_states,
             attentions=outputs.attentions,
         )
+
+
+class PromptForTokenClassification(PreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        
+        self.transformer =  AutoModel.from_pretrained(config._name_or_path)
+        
+        self.dropout = torch.nn.Dropout(config.hidden_dropout)
+        #prefix_ids = config.tokenizer(config.prefix, return_tensors='pt')['input_ids']
+        #print('prefix_ids', prefix_ids)
+        self.score = torch.nn.Linear(config.hidden_size, config.num_labels)
+
+        for param in self.transformer.parameters():
+            param.requires_grad = False
+
+        self.n_layer = config.num_hidden_layers
+        self.n_head = config.n_head
+        self.n_embd = config.hidden_size // config.n_head
+
+        #print('self.prefix_ids', self.prefix_ids)
+        self.prompt_encoder = PromptEncoder(config, self.transformer.word_embeddings)
+        self.config = config
+
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        batch_size = input_ids.shape[0]
+        raw_tokens_embedding = self.transformer.word_embeddings(input_ids)
+        #print('prefix_ids', prefix_ids)
+        prompts =  self.prompt_encoder(self.transformer.device, batch_size)
+        #print('prompts', prompts.shape)
+        #print('raw_tokens_embedding', raw_tokens_embedding)
+        #print('batch_size', batch_size, self.pre_seq_len)
+        inputs_embeds = torch.cat((prompts, raw_tokens_embedding), dim=1)
+        prompt_attention_mask = torch.ones(batch_size, prompts.shape[1]).to(self.transformer.device)
+        attention_mask = torch.cat((prompt_attention_mask, attention_mask), dim=1)
+
+        outputs = self.transformer(
+            # input_ids,
+            attention_mask=attention_mask,
+            # token_type_ids=token_type_ids,
+            # position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            # past_key_values=past_key_values,
+        )
+
+
+        hidden_states = outputs[0][:, prompts.shape[1]:, :]
+        #print('hidden_states', hidden_states.shape)
+        #print('labels', labels.shape)
+        
+        hidden_states = self.dropout(hidden_states)
+        logits = self.score(hidden_states)
+
+        loss = None
+        if labels is not None:
+            # move labels to correct device to enable model parallelism
+            labels = labels.to(logits.device)
+            batch_size, seq_length = labels.shape
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(
+                logits.view(batch_size * seq_length, self.num_labels), labels.view(batch_size * seq_length)
+            )
+
+        if not return_dict:
+            output = (logits,) + transformer_outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return TokenClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+
+class PrefixForTokenClassification(PreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        
+        self.transformer =  AutoModel.from_pretrained(config._name_or_path)
+        
+        self.dropout = torch.nn.Dropout(config.hidden_dropout)
+        self.score = torch.nn.Linear(config.hidden_size, config.num_labels)
+
+        for param in self.transformer.parameters():
+            param.requires_grad = False
+
+        self.n_layer = config.num_hidden_layers
+        self.n_head = config.n_head
+        self.n_embd = config.hidden_size // config.n_head
+        config.n_embd=self.n_embd
+
+        #print('self.prefix_ids', self.prefix_ids)
+        self.prompt_encoder = PrefixEncoder(config, self.transformer)
+        self.config = config
+
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        batch_size = input_ids.shape[0]
+        
+        #print('prefix_ids', prefix_ids)
+        past_key_values, pre_length =  self.prompt_encoder(self.transformer.device, batch_size)
+        #print('prompts', prompts.shape)
+        #print('raw_tokens_embedding', raw_tokens_embedding)
+        #print('batch_size', batch_size, self.pre_seq_len)
+        #inputs_embeds = torch.cat((prompts, raw_tokens_embedding), dim=1)
+        prompt_attention_mask = torch.ones(batch_size, pre_length).to(self.transformer.device)
+        attention_mask = torch.cat((prompt_attention_mask, attention_mask), dim=1)
+
+        outputs = self.transformer(
+            input_ids,
+            attention_mask=attention_mask,
+            return_dict=return_dict,
+            past_key_values=past_key_values,
+        )
+
+        
+        hidden_states = self.dropout(outputs[0])
+
+        logits = self.score(hidden_states)
+
+        loss = None
+        if labels is not None:
+            # move labels to correct device to enable model parallelism
+            labels = labels.to(logits.device)
+            batch_size, seq_length = labels.shape
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(
+                logits.view(batch_size * seq_length, self.num_labels), labels.view(batch_size * seq_length)
+            )
+
+        if not return_dict:
+            output = (logits,) + transformer_outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return TokenClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
